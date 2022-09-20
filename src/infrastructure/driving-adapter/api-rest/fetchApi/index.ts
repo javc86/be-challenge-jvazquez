d691a526b5019e0fb5608637487424d15aa69040
Johander Vazquez <johander.vazquez@gmail.com>
2022-09-19 22:10:34 -0300

    ADD axios-rate-limit

--- src/infrastructure/driving-adapter/api-rest/fetchApi/index.ts
+++ src/infrastructure/driving-adapter/api-rest/fetchApi/index.ts
@@ -1,11 +1,14 @@
 import axios, { AxiosResponse } from 'axios';
 import * as dotenv from 'dotenv';
+import rateLimit from 'axios-rate-limit';
 
 dotenv.config();
 
 axios.defaults.baseURL = process.env.API_BASE_URI
 axios.defaults.headers.common['Content-type'] = 'application/json';
 axios.defaults.headers.common['X-Auth-Token'] = process.env.API_TOKEN || '';
+
+const http = rateLimit(axios.create(), { maxRequests: 2, perMilliseconds: 60000, maxRPS: 2 })
 
 interface FetchApi {
   path: string;
@@ -16,5 +19,5 @@ export default async ({
   path,
   method,
 }: FetchApi): Promise<AxiosResponse> => {
-  return await axios[method](path);
+  return await http[method](path);
 }
